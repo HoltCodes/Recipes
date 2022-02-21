41bd8f4163d892c02cbc890341abbfe31e8b217d
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-02-20 17:19:29 -0800

    commit

--- Recipes/flask_app/models/user.py
+++ Recipes/flask_app/models/user.py
@@ -1,10 +1,8 @@
-from flask_app.config.mysqlconnection import MySQLConnection, connectToMySQL
-from flask_app import app
-from flask import flash, session
-import re
-from flask_bcrypt import Bcrypt
-
-bcrypt = Bcrypt(app)
+from flask_app.config.mysqlconnection import connectToMySQL
+import re	# the regex module
+# create a regular expression object that we'll use later   
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from flask import flash
 
 class User:
     db_name = "recipes"
